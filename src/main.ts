30a2e1702223fddda13250ac16578963548d68fb
Евгений Бученков <evgeniy.buchenkov@rossko.ru>
2022-10-16 22:07:57 +0300

    project recreate

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
-import { ValidationPipe } from '@nestjs/common';
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./app.module";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {ValidationPipe} from "./pipes/validation.pipe";
 
-async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('pug');
+async function start() {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule)
 
-  app.useGlobalPipes(new ValidationPipe());
+    const config = new DocumentBuilder()
+        .setTitle('TestWeb')
+        .setDescription('Документация REST API')
+        .setVersion('1.0.0')
+        .build()
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api/docs', app, document)
 
-  const PORT = process.env.PORT;
-  await app.listen(PORT || 3000);
+    app.useGlobalPipes(new ValidationPipe())
+
+    await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
 }
-bootstrap();
+
+start()
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,50 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { User } from './entities/user.entity';
-import { AddUserDto } from './DTO/add-user.dto';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {Body, Controller, Get, Post, UseGuards} from '@nestjs/common';
+import {CreateUserDto} from "./dto/create-user.dto";
+import {UsersService} from "./users.service";
+import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
+import {User} from "./users.model";
+import {Roles} from "../auth/roles-auth.decorator";
+import {RolesGuard} from "../auth/roles.guard";
+import {AddRoleDto} from "./dto/add-role.dto";
+import {BanUserDto} from "./dto/ban-user.dto";
 
 @ApiTags('Пользователи')
 @Controller('users')
 export class UsersController {
-  constructor(private userService: UsersService) {}
 
-  @ApiOperation({ summary: 'Получение всех зарегестрированных пользователей' })
-  @ApiResponse({
-    status: 200,
-    type: [User],
-    description: 'Пользователи найдены',
-  })
-  @ApiResponse({ status: 400, description: 'Пользователи не найдены' })
-  @Get()
-  async getAllUsers(): Promise<User[]> {
-    return await this.userService.getAllUsers();
-  }
+    constructor(private usersService: UsersService) {}
 
-  @ApiOperation({ summary: 'Добавление пользователя' })
-  @ApiResponse({
-    status: 200,
-    description: 'Пользователь добавлен',
-  })
-  @ApiResponse({ status: 400, description: 'Неудалось добавить пользователя' })
-  @Post('add-user')
-  async addUser(@Body() userDto: AddUserDto) {
-    await this.userService.addUser(userDto);
-  }
+    @ApiOperation({summary: 'Создание пользователя'})
+    @ApiResponse({status: 200, type: User})
+    @Post()
+    create(@Body() userDto: CreateUserDto) {
+        return this.usersService.createUser(userDto);
+    }
+
+    @ApiOperation({summary: 'Получить всех пользователей'})
+    @ApiResponse({status: 200, type: [User]})
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    @Get()
+    getAll() {
+        return this.usersService.getAllUsers();
+    }
+
+    @ApiOperation({summary: 'Выдать роль'})
+    @ApiResponse({status: 200})
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    @Post('/role')
+    addRole(@Body() dto: AddRoleDto) {
+        return this.usersService.addRole(dto);
+    }
+
+    @ApiOperation({summary: 'Забанить пользователя'})
+    @ApiResponse({status: 200})
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    @Post('/ban')
+    ban(@Body() dto: BanUserDto) {
+        return this.usersService.ban(dto);
+    }
 }
