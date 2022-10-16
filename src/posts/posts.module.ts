be651f28c738050260457d55d597837206d213d2
Евгений Бученков <evgeniy.buchenkov@rossko.ru>
2022-10-16 22:08:27 +0300

    msg

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,14 +1,19 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { UsersModule } from "src/users/users.module";
-import { Post } from "./entities/post.entity";
-import { PostsController } from "./posts.controller";
-import { PostsService } from "./posts.service";
+import { Module } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { PostsController } from './posts.controller';
+import {SequelizeModule} from "@nestjs/sequelize";
+import {User} from "../users/users.model";
+import {Role} from "../roles/roles.model";
+import {UserRoles} from "../roles/user-roles.model";
+import {Post} from "./posts.model";
+import {FilesModule} from "../files/files.module";
 
 @Module({
-    controllers: [PostsController],
-    providers: [PostsService],
-    imports: [TypeOrmModule.forFeature([Post]), UsersModule],
-    exports: [PostsService],
-  })
-  export class PostsModule {}
+  providers: [PostsService],
+  controllers: [PostsController],
+  imports: [
+    SequelizeModule.forFeature([User, Post]),
+      FilesModule
+  ]
+})
+export class PostsModule {}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,74 +1,18 @@
-import { Injectable, HttpException, HttpStatus } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { ValidationException } from "src/exceptions/validation.exception";
-import { User } from "src/users/entities/user.entity";
-import { Repository } from "typeorm";
-import { AddPostDto } from "./DTO/add-post.dto";
-import { Post } from "./entities/post.entity";
+import { Injectable } from '@nestjs/common';
+import {CreatePostDto} from "./dto/create-post.dto";
+import {InjectModel} from "@nestjs/sequelize";
+import {Post} from "./posts.model";
+import {FilesService} from "../files/files.service";
 
 @Injectable()
 export class PostsService {
-  constructor(
-    @InjectRepository(Post) private PostRepository: Repository<Post>,
-  ) {}
 
-  async getAllPosts(): Promise<Post[]> {
-    return await this.PostRepository
-      .find({
-        loadRelationIds: true,
-      })
-      .catch((e) => {
-        throw new ValidationException(
-          'Не удалось получить посты'
-        );
-      });
-  }
+    constructor(@InjectModel(Post) private postRepository: typeof Post,
+                private fileService: FilesService) {}
 
-  async getAllUserPosts(userId: number): Promise<Post[]> {
-    return await this.PostRepository
-      .find({
-        where: { authorId: userId},
-        loadRelationIds: true,
-      })
-      .catch((e) => {
-        throw new ValidationException(
-          'Не удалось получить посты пользователя'
-        );
-      });
-  }
-
-  async addPost(PostDto: AddPostDto, user: User): Promise<Post> {
-    if (user == undefined)
-        throw new ValidationException(
-          'Не удалось создать пост'
-        );
-
-    const newPost = await this.PostRepository.create({
-        ...PostDto,
-        author: user
-    });
-    return this.getPostById(newPost.id);
-  }
-
-  async getPostById(id: number) {
-    if (id === undefined) {
-      throw new ValidationException(
-        'Такого поста не существует'
-      );
-    }
-    const post = await this.PostRepository.findOne({
-      where: { id },
-      relations: {
-        author: true,
-      },
-    });
-
-    if (post) {
+    async create(dto: CreatePostDto, image: any) {
+        const fileName = await this.fileService.createFile(image);
+        const post = await this.postRepository.create({...dto, image: fileName})
         return post;
     }
-
-    throw new ValidationException(
-        'Такого поста не существует'
-      );
-  }
 }
