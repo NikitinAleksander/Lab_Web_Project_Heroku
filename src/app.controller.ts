ff097fe9ee2950127249ca8835022656962b36c4
Евгений Бученков <evgeniy.buchenkov@rossko.ru>
2022-10-17 14:03:13 +0300

    axios fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get, Render } from "@nestjs/common";
-import axios from "axios";
 
 @Controller()
 export class AppController {
   @Get()
   @Render('index')
   public async index() {
-    var posts = await axios.get("http://localhost:5000/posts");
-    return {posts: posts.data};
+    return {posts: []};
   }
 }
