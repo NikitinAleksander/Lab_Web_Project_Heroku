a4cb9660dbd50054acc6c75b71cbf9ebb008c3f7
Евгений Бученков <evgeniy.buchenkov@rossko.ru>
2022-10-17 14:26:48 +0300

    fetch fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, Render } from "@nestjs/common";
+import fetch from "node-fetch";
 
 @Controller()
 export class AppController {
   @Get()
   @Render('index')
   public async index() {
-    return {posts: []};
+    const response = await fetch("http://localhost:5000/posts",{method:"GET"});
+    const data = await response.json();
+
+    return {posts: data};
   }
 }
